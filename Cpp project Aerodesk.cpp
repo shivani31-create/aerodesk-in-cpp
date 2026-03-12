549a310ce13109ced07ccd0bb1ccc456fc209bcb
shivani31-create <31shivani1831@gmail.com>
2026-03-12 23:11:59 +0530

    Add files via upload

---
+++ Cpp project Aerodesk.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include<string>
+#include<iomanip>
+#include<stdexcept>
+#include<algorithm>
+#include<map>
+using namespace std;
+
+//CUSTOM EXCEPTIONS
+
+class AerodeskException:public exception
+{
+    protected: string msg;
+    public:
+        explicit AeroDeskException(const string&m):msg(m){}
+        const char*what()const noexcept override{return msg.c_str();
+        }
+};
+
+class FlightNotFounfException:public AeroDeskException
+{
+public:
+    explicit FlightNotFoundException(const string&id)
+    :AeroDeskException("Flight Not Found: " +id){}
+};
+
+class SeatUnavailableException:public AeroDeskException
+{
+ public:
+    explicit SeatUnavailableException(const string&cls)
+    :AeroDeskException("No seats available in "+cls +"class."){}
+};
+
+class BookingNotFoundException:public AeroDeskException
+{
+public:
+    explicit BookingNotFoundException(const string&id)
+    :AeroDeskException("Booking not found: "+id){}
+};
+
+class InvalidInputException:public AeroDeskException
+{
+public:
+    explicit InvalidInputException(const string&detail)
+    :AeroDeskException("Invalid input: "+detail){}
+};
